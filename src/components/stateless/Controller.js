045ccbb507b2c5861404566022720c7be3c1a78d
Deepanshi1523 <104934054+Deepanshi1523@users.noreply.github.com>
2023-10-03 18:03:32 +0530

    made all songs.js

---
+++ src/components/stateless/Controller.js
@@ -0,0 +1,33 @@
+const styles = {
+	controller: {
+		height: 230,
+		width: 230,
+	},
+	forward: {
+		height: 56,
+		width: 60,
+		cursor: "pointer",
+		padding: "10px",
+	},
+	backward: {
+		height: 55,
+		width: 60,
+		transform: "rotate(180deg)",
+		cursor: "pointer",
+		padding: "10px",
+	},
+	menu: {
+		fontWeight: "bolder",
+		fontSize: 27,
+		cursor: "pointer",
+		padding: "5px",
+	},
+	resume: {
+		height: "50px",
+		width: "60px",
+		cursor: "pointer",
+		padding: "12px",
+	},
+};
+
+export default Controller;
